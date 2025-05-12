12663068248a19101a6f44b30b7caec9cca9daad
Rowan45674567 <117251113+Rowan45674567@users.noreply.github.com>
2025-05-12 14:12:32 -0700

    Update encoder.cpp

--- encoder.cpp
+++ encoder.cpp
@@ -7,15 +7,14 @@ using namespace std;
 int main()
 {
     wiringPiSetup();			// Setup the library
-    pinMode(15, INPUT);		// Configure GPIO8 as an input
-    pinMode(0, INPUT);		// Configure GPIO11 as an input
 
-// Main program loop
-while(1)
-{
-    cout << "pin 8: " << digitalRead(15) << endl; 
-    cout << "pin 11: " << digitalRead(0)  << endl;
-}
+    while(1)
+    {
+    pinMode(15, INPUT);		// Configure GPIO8 as an input
+    pinMode(16, INPUT);		// Configure GPIO11 as an input
+    cout << "pin 8: " << digitalRead(15) << " " << "pin 11: " << digitalRead(16)  << endl;
+
+    
+    }
 	return 0;
 }
-
